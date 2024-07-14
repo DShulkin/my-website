d88a7f3f33ed7a15f3b56aa6be9b01e757dc34f2
David Shulkin <thedavidshulkin@gmail.com>
2024-07-14 01:13:36 -0700

    homepage modifications

--- src/App.js
+++ src/App.js
@@ -4,12 +4,11 @@ import Home from './components/Home'
 import About from './components/About'
 import Projects from './components/Projects'
 import Contact from './components/Contact'
-import './App.css'
 
 
 function App() {
   return (
-    <div className="mainContainer">
+    <>
       <Navbar />
       <div className="allSections">
         <Home />
@@ -17,7 +16,7 @@ function App() {
         <Projects />
         <Contact />
       </div>
-    </div>
+    </>
   )
 }
 
