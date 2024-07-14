c72da539294cfae799511c0aad7bffe0578c1bdd
David Shulkin <thedavidshulkin@gmail.com>
2024-07-14 01:26:17 -0700

    Rename navbar.css to Navbar.css

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
-import d_logo from '../assets/icons/d_logo.svg';
-import hamburgerMenu_offBlack from '../assets/icons/hamburgerMenu_offBlack.svg';
-import '../Navbar.css';
+import React, { useState } from 'react'
+import d_logo from '../assets/icons/d_logo.svg'
+import hamburgerMenu_offBlack from '../assets/icons/hamburgerMenu_offBlack.svg'
+import '../Navbar.css'
 
 function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
