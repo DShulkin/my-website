aa726e6ec2b6daeb35f2f13325b6dc467dacab57
David Shulkin <thedavidshulkin@gmail.com>
2025-06-07 20:32:36 -0700

    hook test 11

--- src/hooks/useBreakpoint.js
+++ src/hooks/useBreakpoint.js
@@ -4,16 +4,16 @@ export default function useBreakpoint() {
     const [width, setWidth] = useState(null)
 
     useEffect ( () => {
-        const handleResize = () => setWidth(window.innerWidth) /* get current width of browser window */
-        handleResize() /* immediately called”*/
-        window.addEventListener('resize', handleResize) /* Every time the user resizes the window, run handleResize again.”*/
-        return () => window.removeEventListener('resize', handleResize) /* clean up code prevents bugs */
+        const handleResize = () => setWidth(window.innerWidth)
+        handleResize()
+        window.addEventListener('resize', handleResize)
+        return () => window.removeEventListener('resize', handleResize)
     }, [])
 
     return {
         isMobile: width !== null && width < 768,
-        isTablet: width >= 768 && width < 1440,
-        isDesktop: width >= 1440
+        isTablet: width >= 768 && width < 1180,
+        isDesktop: width >= 1180
     }
 }
 
