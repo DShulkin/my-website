63b4d8598f7d7508c1ec7b713b9076fa45a3c3de
David Shulkin <thedavidshulkin@gmail.com>
2025-06-07 20:03:58 -0700

    hook test 7

--- src/hooks/useBreakpoint.js
+++ src/hooks/useBreakpoint.js
@@ -12,8 +12,8 @@ export default function useBreakpoint() {
 
     return {
         isMobile: width !== null && width < 768,
-        isTablet: width >= 768 && width < 1440,
-        isDesktop: width >= 1440
+        isTablet: width >= 768 && width < 1600,
+        isDesktop: width >= 1600
     }
 }
 
